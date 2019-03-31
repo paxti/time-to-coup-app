e28b29d1fee4cb4b8b8b02718ad9f26740c259b7
paxti <iurii.tverezovskyi@gmail.com>
2019-03-30 20:07:42 -0500

    Fix styling for tests

--- __tests__/containers/MainContainer.test.js
+++ __tests__/containers/MainContainer.test.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { View, FlatList, Text } from "react-native";
-import { shallow } from "enzyme";
-import configureStore from "redux-mock-store";
+import React from 'react';
+import { View, FlatList, Text } from 'react-native';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
-import MainContainer from "../../src/containers/MainContainer";
+import MainContainer from '../../src/containers/MainContainer';
 
-describe("Main Container", () => {
+describe('Main Container', () => {
   let store;
   let testObject;
 
@@ -15,24 +15,24 @@ describe("Main Container", () => {
     testObject = shallow(<MainContainer />, { context: { store } }).dive();
   });
 
-  it("should have a View", () => {
+  it('should have a View', () => {
     expect(testObject.find(View).length).toBe(1);
   });
 
-  it("should have Flat list", () => {
+  it('should have Flat list', () => {
     expect(testObject.find(FlatList).length).toBe(1);
   });
 
-  describe("Flat List", () => {
-    it("should have data by default", () => {
+  describe('Flat List', () => {
+    it('should have data by default', () => {
       expect(testObject.find(FlatList).props().data.length).not.toEqual(0);
     });
 
-    it("should have renderItem props", () => {
+    it('should have renderItem props', () => {
       expect(testObject.find(FlatList).props().renderItem).not.toBeUndefined();
     });
 
-    it("should use Text as renderItem", () => {
+    it('should use Text as renderItem', () => {
       expect(
         testObject
           .find(FlatList)
@@ -41,7 +41,7 @@ describe("Main Container", () => {
       ).toEqual(<Text />);
     });
 
-    it("should render data from JSON object", () => {
+    it('should render data from JSON object', () => {
       const object = { item: { text: 1 } };
       expect(
         testObject
@@ -51,24 +51,20 @@ describe("Main Container", () => {
       ).toEqual(<Text>{object.item.text}</Text>);
     });
 
-    it("should render data from props JSON array", () => {
+    it('should render data from props JSON array', () => {
       const array = [{ text: 1 }, { text: 5 }, { text: 3 }, { text: 2 }];
       testObject = shallow(<MainContainer data={array} />, {
         context: { store }
       }).dive();
-      expect(testObject.find(FlatList).props().data.length).toEqual(
-        array.length
-      );
+      expect(testObject.find(FlatList).props().data.length).toEqual(array.length);
     });
 
-    it("should render data from props JSON array with 2 items", () => {
+    it('should render data from props JSON array with 2 items', () => {
       const array = [{ text: 1 }, { text: 5 }];
       testObject = shallow(<MainContainer data={array} />, {
         context: { store }
       }).dive();
-      expect(testObject.find(FlatList).props().data.length).toEqual(
-        array.length
-      );
+      expect(testObject.find(FlatList).props().data.length).toEqual(array.length);
     });
   });
 });
