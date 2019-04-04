bdb5583557149e796715254d487aee7d2a47678a
geosef <joe@geosef.io>
2019-04-04 04:06:16 -0500

    Added apollo client and provider to root to connect to graphql

--- src/root.js
+++ src/root.js
@@ -8,17 +8,22 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import configureStore from './store/configure-store';
 import App from './containers/app';
 
+const client = new ApolloClient({ uri: 'https://time-to-coup.appspot.com/graphql' });
 const store = configureStore({
   sessionActive: false
 });
 
 const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>
 );
 
 export default Root;
