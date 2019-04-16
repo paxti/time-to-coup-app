5cde70de305045a0cd3393db49ce7012f043dc57
paxti <iurii.tverezovskyi@gmail.com>
2019-04-15 23:11:05 -0500

    Move InactiveSessionContainer into separate file

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import PropTypes from 'prop-types';
-import CardsContainer from './CardsContainer';
-import SessionContainer from './SessionContainer';
-import LeaderboardContainer from './LeaderboardContainer';
 import ActiveSessionContainer from './ActiveSessionContainer';
-
-const InactiveSessionContainer = createAppContainer(
-  createBottomTabNavigator({
-    Cards: {
-      screen: CardsContainer
-    },
-    Session: {
-      screen: SessionContainer
-    },
-    Leaderboard: {
-      screen: LeaderboardContainer
-    }
-  })
-);
+import InactiveSessionContainer from './InactiveSessionContainer';
 
 const App = ({ sessionActive = false }) => {
   return sessionActive ? <ActiveSessionContainer /> : <InactiveSessionContainer />;
